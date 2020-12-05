66b2abe0afcc76a837cabcc57edd27728ea5172f
Hari Om Batra <ohari5336@gmail.com>
2020-12-05 10:55:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var NSEAPI = API.NSE;
 
 var app = express();
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("Server running on port 3000");
 });
 
